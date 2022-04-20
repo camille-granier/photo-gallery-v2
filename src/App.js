967eb8f3a39ac6267d706f4ebe2d7098453360c3
camille-granier <granierc@protonmail.com>
2022-04-20 11:57:58 +1000

    initialize project

--- src/App.js
+++ src/App.js
@@ -1,25 +1,33 @@
-import logo from './logo.svg';
-import './App.css';
+import { useEffect, useState } from 'react';
+import React from 'react';
+import axios from 'axios';
+import Form from './components/Form';
+import Card from './components/Card';
 
-function App() {
+const App = () => {
+
+  const [picsData, seetPicsData] = useState([]);
+
+  useEffect(() => {
+    axios
+      .get('http://localhost:5000/pictures')
+      .then((res) => seetPicsData(res.data));
+  }, [])
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <h1>Photo Gallery</h1>
+      <Form />
+      <div className='cards-container'>
+        {picsData?.map((pic, index) => (
+          <Card key={index} pic={pic} />
+          )
+        )}
+      </div>
+    </>
   );
-}
+};
 
 export default App;
+
+
---
+++ src/components/Card.jsx
@@ -0,0 +1,55 @@
+import React from 'react';
+import { useRef, useState } from 'react';
+import axios from 'axios';
+import Delete from './Delete';
+import { FaRegEdit } from 'react-icons/fa';
+
+const Card = ({pic}) => {
+
+    const [edit, setEdit] = useState(false);
+    const artistInput = useRef();
+
+    const handleEdit = () => {
+        setEdit(false);
+
+        const data = {
+            artist: artistInput.current.value,
+            year: pic.year,
+            photo: pic.photo
+        };
+        axios.put("http://localhost:5000/pictures/" + pic.id, data);
+    }
+
+    return (
+        <div className='card'>
+            <img src={pic.photo} alt={"picture of" + pic.artist} />
+            <div className='infos'>
+                <div className='title'>
+                    {edit?(
+                        <div>
+                            <input
+                            defaultValue={pic.artist}
+                            ref={artistInput}
+                            autofocus></input>
+                        <button onClick={() => handleEdit()}>Validate</button>
+                        </div>
+                    )
+                    : (
+                        <h3>
+                            {artistInput.current ? artistInput.current.value : pic.artist}
+                        </h3>
+                    )}
+                <p>{pic.year}</p>
+            </div>
+            <div className='button-container'>
+                <div className='edit-icon' onClick={() => setEdit(!edit)}>
+                        <FaRegEdit />
+                </div>
+                <Delete id={pic.id}/>
+            </div>
+            </div>     
+        </div>
+    );
+};
+
+export default Card;

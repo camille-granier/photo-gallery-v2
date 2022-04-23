dd3ff59cab5bd86d8754f7848a68847f59d439e6
camille-granier <granierc@protonmail.com>
2022-04-23 19:04:34 +1000

    getting photos from store in hero component

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,10 +1,19 @@
 import React from 'react';
+import { useSelector } from 'react-redux';
 
 const Hero = () => {
+
+  const pictures = useSelector((state) => state.pictures.pictures);
+
     return (
       <section className="hero">
-        <h1>Explore the most beautiful photos</h1>
-        <div className='photo-slider'></div>
+        <h1>Get inspired </h1>
+        <div className='photo-slider'>
+          <img src={pictures[0].photo} alt="kk"/>
+          {pictures?.map((pic, index) => (
+            <img src={pic.photo} alt={index} key={index}/>
+          ))}
+        </div>
       </section>
     );
 };

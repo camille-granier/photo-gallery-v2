fa066d6cb8a412b3070b479b7575fb3c9e20ac65
camille-granier <granierc@protonmail.com>
2022-04-27 16:02:32 +1000

    styling contact page for mobile, tablet and desktop

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -4,8 +4,11 @@ const Contact = () => {
     return (
         <div className='contact' id="contact">
             <h2>Get in touch</h2>
-            <p className='email'>graniec@protonmail.com</p>
+            <div className='links'>
+            <p className='email'><span className='email-span'>granierc@protonmail.com</span></p>
             <a href="https://github.com/camille-granier" className='github-link' target="_blank" rel="noreferrer">Github</a>
+            </div>
+            <p className='dev'>Developped by Camille Granier</p>
         </div>
     );
 };

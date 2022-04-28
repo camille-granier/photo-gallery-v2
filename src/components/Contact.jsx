ff7c0e29e7f802828c4ae6d88884cb18e8c39d0e
camille-granier <granierc@protonmail.com>
2022-04-28 15:56:59 +1000

    adding scrolling to contact section with react-scroll

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -2,14 +2,14 @@ import React from 'react';
 
 const Contact = () => {
     return (
-        <div className='contact' id="contact">
+        <section className='contact' id="contact" name="contact">
             <h2>Get in touch</h2>
             <div className='links'>
             <p className='email'><span className='email-span'>granierc@protonmail.com</span></p>
             <a href="https://github.com/camille-granier" className='github-link' target="_blank" rel="noreferrer">Github</a>
             </div>
-            <p className='dev'>Developped by Camille Granier</p>
-        </div>
+            <p className='dev'>Developed by Camille Granier</p>
+        </section>
     );
 };
 
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,15 +1,20 @@
 import React from 'react';
 import { ReactComponent as Logo } from '../assets/pictures-logo.svg';
 import { FiGithub, FiAtSign } from "react-icons/fi";
+import {Link, animateScroll as scroll } from "react-scroll";
 
 const Header = () => {
+
+
     return (
         <header className='header'>
             <div className='header-container'>
                 <Logo className="logo" height="20px"/>    
                 <div className="icons">
                     <a className="github" href="https://github.com/camille-granier" rel="noreferrer" target="_blank"><FiGithub/></a>
-                    <a className="email" href="#contact" target="_blank"><FiAtSign/></a>
+                    <Link className="email" 
+                          to="contact" 
+                          smooth={true}><FiAtSign/></Link>
                 </div>
             </div>
         </header>

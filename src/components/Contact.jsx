e8a3798a623e7e34f39bcd8f61b40490834af552
camille-granier <granierc@protonmail.com>
2022-05-01 12:13:22 +1000

    github-link hover effect

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -5,8 +5,15 @@ const Contact = () => {
         <section className='contact' id="contact" name="contact">
             <h2>Get in touch</h2>
             <div className='links'>
-            <a className='email' href="mailto: granierc@protonmail.com"><span className='email-span'>granierc@protonmail.com</span></a>
-            <a href="https://github.com/camille-granier" className='github-link' target="_blank" rel="noreferrer">Github</a>
+                <a className='email' href="mailto: granierc@protonmail.com"><span className='email-span'>granierc@protonmail.com</span></a>
+                <a href="https://github.com/camille-granier" className='github-link' target="_blank" rel="noreferrer">
+                    <span className="mask">
+                        <div className='github-link-container'>
+                            <span className="link-title-1 github-title">Github</span>
+                            <span className="link-title-2 github-title">Github</span>
+                        </div>
+                    </span>
+                </a>
             </div>
             <p className='dev'>Developed by Camille Granier</p>
         </section>

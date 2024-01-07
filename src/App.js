864142576081c930fad3c83c8c5c237065d0cdfc
camille-granier <granierc@protonmail.com>
2024-01-07 18:46:23 +1100

    correction of H1 mask breakpoint for responsiveness

--- src/App.js
+++ src/App.js
@@ -18,7 +18,7 @@ const App = () => {
 
   const getPictures = () => {
     axios
-       .get('https://pictures-photo-gallery.herokuapp.com/pictures')
+       .get('http://localhost:5000/pictures')
        .then((res) => {
          dispatch(setPicturesData(res.data));
           setIsLoading(false)});          
